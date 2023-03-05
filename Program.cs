47583d9ed6279d2d62cc2e4531f56f3af7cdad2b
Lcuky12 <118757873+Lcuky12@users.noreply.github.com>
2023-03-05 21:18:11 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int multiple = 3;
+            int multiple2 = 5;
+            int minimumRandomValue = 0;
+            int maximumRandomValue = 101;
+            int maxNumber = random.Next(minimumRandomValue, maximumRandomValue);
+            int sum = 0;
+
+            for (int i = multiple; i <= maxNumber; i++)
+            {
+                if (i % multiple == 0 && i % multiple2 == 0)
+                {
+                    sum += i;
+                }
+            }
+           
+            Console.WriteLine(" Рандомное число " + maxNumber + " Сумма всех чисел кратных " + multiple + " или " + multiple2 + " до " + maxNumber + " равна " + sum);
+        }
+    }
+}
